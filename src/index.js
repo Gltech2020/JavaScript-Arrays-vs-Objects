c6d69cd1dea5bfb67ab6f5a716f01598f86ad209
Gladys <77966043+Gltech2020@users.noreply.github.com>
2024-04-29 00:20:09 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+console.log(paris.temperature);
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 24,
+  capitalCity: false,
+};
+console.log(sydney.temperature);
+
+let australianCities = ["Vancouver", "Ottawa", "Canberra"];
+console.log(australianCities[0]);
+console.log(australianCities[1]);
+console.log(australianCities[2]);
+let cities = [
+  {
+    name: "Harare",
+    country: "Zimbabwe",
+    language: "English",
+    temperature: 28,
+    capitalCity: true,
+  },
+  {
+    name: "Pretoria",
+    country: "South Africa",
+    language: "English",
+    temperature: 25,
+    capitalCity: true,
+  },
+  {
+    name: "Hamburg",
+    country: "Germany",
+    language: "German",
+    temperature: 18,
+    capitalCity: false,
+  },
+];
+console.log(cities[0].temperature);
+console.log(cities[1].temperature);
+console.log(cities[2].temperature);
